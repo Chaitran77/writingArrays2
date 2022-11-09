6553ab1e5ceea87a21e8d355b8050fdb0daa8109
Chaitran77 <kiran@inteleyes.com>
2022-11-09 16:10:28 +0000

    cleaned up

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        lastCharRemover(new String[]{"hi", "some", "thing", "here"});
+    }
+
+    public static void replaceAll(int[] numbers, int num) {
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] % 2 == 1) { continue; }
+            numbers[i] = 3;
+        }
+    }
+
+    public static void lastCharRemover(String[] names) {
+        for (int i = 0; i < names.length; i++) { names[i] = names[i].substring(0, names[i].length()-1); }
+//      System.out.println(Arrays.toString(names));
+
     }
 }
